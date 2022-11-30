f8eaa2a4ab4c5165457dabcc2a024ea585c16f63
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-30 15:42:39 -0300

    Update autenticação Google

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,6 +1,9 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google';
 import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
 
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
     name: string;
@@ -9,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
     user: UserProps;
+    isUserLoading: boolean;
     signIn: () => Promise<void>;
 }
 
@@ -21,16 +25,31 @@ export const AuthContext = createContext({} as AuthContextDataProps);
 
 
 export function AuthContextProvider({ children }: AuthProviderProps){
+    const [isUserLoading, setIsUserLoading] = useState(false);
 
-    console.log(AuthSession.makeRedirectUri({ useProxy: true }));
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        clientId: '257905569756-va3gn4vso2b1bkg4bv85ndtk7smg48g8.apps.googleusercontent.com',
+        redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+        scopes: ['profile', 'email']
+    });
+
 
     async function signIn(){
-        console.log("sapup3 na area");
+        try{
+            setIsUserLoading(true);
+            await promptAsync();
+        }catch (error){
+            console.log(error);
+            throw error;
+        }finally {
+            setIsUserLoading(false);
+        }
     }
 
     return (
         <AuthContext.Provider value={{
             signIn,
+            isUserLoading,
             user: {
                 name: 'Daniel Oliveira',
                 avatarUrl: 'https://github.com/daniel-oliv3.png'
